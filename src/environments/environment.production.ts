e025fb80e21ead93f2d51bb14eb044ba147d2efb
sergmain <serg_main@yahoo.com>
2019-09-21 22:40:39 -0700

    .

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export let environment: { baseUrl: string; production: boolean };
 environment = {
     production: true,
-    baseUrl: 'https://cons-411.herokuapp.com/rest/v1/',
+    baseUrl: 'https://cons-41111.herokuapp.com/rest/v1/',
 };
