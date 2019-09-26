98b61f7ac85261f6826979c8143d915060a8a584
petspets <loopi@ya.ru>
2019-09-26 17:19:52 +0800

    up accounts
    cleaning

--- src/app/services/accounts/accounts.service.ts
+++ src/app/services/accounts/accounts.service.ts
@@ -1,45 +1,65 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { generateFormData } from '@src/app/helpers/generateFormData';
+import { environment } from '@src/environments/environment';
 import { Observable } from 'rxjs';
-import { urls } from './urls';
 
 @Injectable({ providedIn: 'root' })
 export class AccountsService {
-    constructor(private http: HttpClient) {}
+    POST: any;
+    GET: any;
+    formData: any;
+
+    constructor(private http: HttpClient) {
+        const base: any = (url: string): string => `${environment.baseUrl}launchpad/account${url}`;
+        this.POST = (url: string, data: any): Observable < any > => this.http.post(base(url), data);
+        this.GET = (url: string, options: any = {}): Observable < any > => this.http.get(base(url), options);
+        this.formData = generateFormData;
+    }
 
     accounts = {
-        get: (page: number): Observable < object > => this.http.get(urls.accounts.get(page))
+        // @GetMapping("/accounts")
+        // public AccountData.AccountsResult accounts(@PageableDefault(size = 5) Pageable pageable) {
+        //     return accountTopLevelService.getAccounts(pageable);
+        // }
+        get: (page: number): Observable < object > =>
+            this.GET(`/accounts?page=${page}`)
     };
 
     account = {
+        // @GetMapping(value = "/account/{id}")
+        // public AccountData.AccountResult getAccount(@PathVariable Long id) {
+        //     return accountTopLevelService.getAccount(id);
+        // }
         get: (id: string | number): Observable < object > =>
-            this.http.get(urls.account.get(id)),
+            this.GET(`/account/${id}`),
 
-        addCommit: (data: object): Observable < object > => {
-            return this.http.post(urls.account.addCommit(), data);
-        },
+        // @PostMapping("/account-add-commit")
+        // public OperationStatusRest addFormCommit(@RequestBody Account account) {
+        //     return accountTopLevelService.addAccount(account);
+        // }
+        addCommit: (account: object): Observable < object > =>
+            this.POST(`/account-add-commit`, account),
 
-        editCommit: (id: string, publicName: string, enabled: boolean): Observable < object > => {
-            const formData: FormData = new FormData();
-            formData.append('id', id);
-            formData.append('publicName', publicName);
-            formData.append('enabled', enabled.toString());
-            return this.http.post(urls.account.editCommit(), formData);
-        },
+        // @PostMapping("/account-edit-commit")
+        // public OperationStatusRest editFormCommit(Long id, String publicName, boolean enabled) {
+        //     return accountTopLevelService.editFormCommit(id, publicName, enabled);
+        // }
+        editCommit: (id: string, publicName: string, enabled: boolean): Observable < object > =>
+            this.POST(`/account-edit-commit`, this.formData({ id, publicName, enabled })),
 
-        passwordEditCommit: (id: string, password: string, password2: string): Observable < object > => {
-            const formData: FormData = new FormData();
-            formData.append('id', id);
-            formData.append('password', password);
-            formData.append('password2', password2);
-            return this.http.post(urls.account.passwordEditCommit(), formData)
-        },
+        // @PostMapping("/account-password-edit-commit")
+        // public OperationStatusRest passwordEditFormCommit(Long id, String password, String password2) {
+        //     return accountTopLevelService.passwordEditFormCommit(id, password, password2);
+        // }
+        passwordEditCommit: (id: string, password: string, password2: string): Observable < object > =>
+            this.POST(`/account-password-edit-commit`, this.formData({ id, password, password2 })),
 
-        roleCommit: (accountId: string, roles: string): Observable < object > => {
-            const formData: FormData = new FormData();
-            formData.append('accountId', accountId);
-            formData.append('roles', roles);
-            return this.http.post(urls.account.roleCommit(), formData)
-        }
+        // @PostMapping("/account-role-commit")
+        // public OperationStatusRest roleFormCommit(Long accountId, String roles) {
+        //     return accountTopLevelService.roleFormCommit(accountId, roles);
+        // }
+        roleCommit: (accountId: string, roles: string): Observable < object > =>
+            this.POST(`/account-role-commit`, this.formData({ accountId, roles }))
     };
 }
--- src/app/services/accounts/index.ts
+++ src/app/services/accounts/index.ts
@@ -2,4 +2,3 @@ export * from './Account';
 export * from './Authoritie';
 export * from './accounts.service';
 export * from './response';
-export * from './urls';
