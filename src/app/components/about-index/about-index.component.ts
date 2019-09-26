a52bf388dac990f08fffe63d72ba289283f03ff2
petspets <loopi@ya.ru>
2019-09-26 17:27:11 +0800

    cleaning

--- src/app/components/about-index/about-index.component.ts
+++ src/app/components/about-index/about-index.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'about-index',
   templateUrl: './about-index.component.pug',
-  styleUrls: ['./about-index.component.sass']
+  styleUrls: ['./about-index.component.scss']
 })
 export class AboutIndexComponent implements OnInit {
 
--- src/app/components/launchpad-index/launchpad-index.component.ts
+++ src/app/components/launchpad-index/launchpad-index.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'launchpad-index',
   templateUrl: './launchpad-index.component.pug',
-  styleUrls: ['./launchpad-index.component.sass']
+  styleUrls: ['./launchpad-index.component.scss']
 })
 export class LaunchpadIndexComponent implements OnInit {
 
