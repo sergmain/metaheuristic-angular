eb763c65aabffdc5d38032a7e345abbe2efab50d
sergmain <serg_main@yahoo.com>
2019-11-09 20:47:13 -0800

    .

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,10 @@
-export let environment: { baseUrl: string; production: boolean };
+export let environment: {
+    baseUrl: string;
+    production: boolean;
+    userLifeTime?: number;
+};
 environment = {
     production: true,
     baseUrl: 'http://localhost:8080/rest/v1/',
+    userLifeTime: 30 * 60 * 1000 // 30 minutes
 };
