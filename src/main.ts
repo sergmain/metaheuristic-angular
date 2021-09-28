dad2a700bfe9e1033c484d0850c345b81024e5dc
sergmain <serg_main@yahoo.com>
2021-09-28 14:55:35 -0700

    .

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'hammerjs';
-
 if (environment.isSslRequired) {
     if (window.location.protocol === 'http:') {
         window.location.href = window.location.href.replace('http', 'https');
