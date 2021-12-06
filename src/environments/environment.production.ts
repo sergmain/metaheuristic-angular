9a6427b8cf94f8665a4581f1031cbe21295cf23d
sergmain <serg_main@yahoo.com>
2021-12-06 00:09:49 -0800

    .

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -8,11 +8,30 @@ export const environment: IEnvironment = {
     isSslRequired: true,
     batchInterval: 15 * 1000, // in milliseconds
     language: 'EN', // other supported languages: 'RU'
-    brandingTitle: 'Branding Title',
-    brandingMsg: '<p>Lorem ipsum <b>dolor</b>, sit amet consectetur adipisicing elit. Aperiam, ' +
-        'libero molestiae! Neque fugiat necessitatibus pariatur aliquid vel rerum ad sequi, ' +
-        'sed beatae hic consequatur similique eveniet reiciendis sapiente soluta explicabo.</p>',
-    brandingMsgIndex: '<p>Lorem ipsum <b>dolor</b>, sit amet consectetur adipisicing elit. Aperiam, ' +
-        'libero molestiae! Neque fugiat necessitatibus pariatur aliquid vel rerum ad sequi, ' +
-        'sed beatae hic consequatur similique eveniet reiciendis sapiente soluta explicabo.</p>',
+    brandingTitle: 'Metaheuristic',
+    brandingMsg: '<b>Metaheuristic platform</b><p>Metaheuristic is an application which implements (or intended to) a Turing complete machine.' +
+        'The main use of MH is a management of distributed tasks. Right now there are two main areas where MH is being used:</p>' +
+        '<ul><li>' +
+        '<p>AI model’s hyper-parameter optimization purpose.</p>' +
+        'Each optimization is presented as Experiment. An Experiment consists of some Tasks. Tasks are created at Dispatcher and distributed to Processor. ' +
+        'For evaluating a performance of models, metrics and other data are collected and evaluated later by Metaheuristic.' +
+        '</li>' +
+        '<li>' +
+        '<p>Batch processing.</p>' +
+        'Common usage of batch processing - split data, create tasks for processing each part of data, process Tasks, aggregate results' +
+        '</li>' +
+        '</ul>'
+    ,
+    brandingMsgIndex: '<b>Metaheuristic platform</b><p>Metaheuristic is an application which implements (or intended to) a Turing complete machine.' +
+        'The main use of MH is a management of distributed tasks. Right now there are two main areas where MH is being used:</p>' +
+        '<ul><li>' +
+        '<p>AI model’s hyper-parameter optimization purpose.</p>' +
+        'Each optimization is presented as Experiment. An Experiment consists of some Tasks. Tasks are created at Dispatcher and distributed to Processor. ' +
+        'For evaluating a performance of models, metrics and other data are collected and evaluated later by Metaheuristic.' +
+        '</li>' +
+        '<li>' +
+        '<p>Batch processing.</p>' +
+        'Common usage of batch processing - split data, create tasks for processing each part of data, process Tasks, aggregate results' +
+        '</li>' +
+        '</ul>',
 };
