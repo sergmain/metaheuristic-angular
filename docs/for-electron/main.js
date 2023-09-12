5c4606bb3aea3a57f91f5a1940798b697977f474
sergmain <serg_main@yahoo.com>
2023-09-12 01:00:23 -0700

    .

--- docs/for-electron/main.js
+++ docs/for-electron/main.js
@@ -1,17 +1,91 @@
+// noinspection NodeCoreCodingAssistance,DuplicatedCode
+
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const http = require('http');
+const fs = require('fs');
+const util = require('util');
+// const {exec: child} = require("child_process");
 
-const child = require('child_process').execFile;
-const executablePath = path.join(__dirname, 'metaheuristic', 'metaheuristic.exe');
 
-child(executablePath, function(err, data) {
-  if(err){
-    console.error(err);
-    return;
-  }
-  console.log(data.toString());
-});
+const userHomePath = path.join(app.getPath('home'), '.metaheuristic', 'electron');
+
+if (!fs.existsSync(userHomePath)){
+    fs.mkdirSync(userHomePath, { recursive: true });
+}
+
+const log_file = fs.createWriteStream(path.join(userHomePath, 'console.log'), {flags: 'a'});
+const log_stdout = process.stdout;
+
+console.log = function(d) {
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
+console.log("Metaheuristic front-end was started at " + new Date());
+
+const port = 64968;
+
+try {
+  const hostname = 'localhost';
+// Creating Server
+  console.log(`Start preparing a Server for starting at http://${hostname}:${port}/`);
+  const server = http.createServer((req,res)=>{
+    // Handling Request and Response
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end("Front-end is alive.")
+  });
+
+// Making the server to listen to required
+// hostname and port number
+  server.listen(port, hostname,()=>{
+    // Callback
+    console.log(`Server is running at http://${hostname}:${port}/`);
+  });
+} catch(e) {
+  console.log(e);
+}
+
+
+/*
+try {
+  spawn('"with spaces.cmd"', ['arg with spaces'], { shell: true });
+
+  // const child = require('child_process').execFile;
+  const childSpawn = require('child_process').spawn;
+  let executablePath = '"'+path.join(__dirname, 'metaheuristic', 'metaheuristic.exe') + '"';
+  console.log("Metaheuristic executablePath: " + executablePath);
+
+  childSpawn('metaheuristic.exe', function(err, data) {
+    if(err){
+      console.error(err);
+      return;
+    }
+    console.log(data.toString());
+  });
+}
+catch(e) {
+  console.log(e);
+}
+*/
+try {
+  const child = require('child_process').exec;
+  let executablePath = '"'+path.join(__dirname, 'metaheuristic', 'metaheuristic.exe') + '"';
+  console.log("Metaheuristic executablePath: " + executablePath);
+
+  child(executablePath, function(err, data) {
+    if(err){
+      console.error(err);
+      return;
+    }
+    console.log(data.toString());
+  });
+}
+catch(e) {
+  console.log(e);
+}
 
 function createWindow () {
   // Window Customization - https://www.electronjs.org/docs/latest/tutorial/window-customization
@@ -25,7 +99,7 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       // nodeIntegration: true,
-      enableRemoteModule: true
+      enableRemoteModule: false
     }
   })
 
@@ -33,7 +107,7 @@ function createWindow () {
   mainWindow.loadFile(path.join(__dirname, 'mh-angular', 'index.html'))
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -50,10 +124,26 @@ app.whenReady().then(() => {
     }
   })
 
+  // Continue to handle mainWindow "close" event here
+/*
+  mainWindow.on('close', function(e){
+    if(!force_quit){
+      e.preventDefault();
+      mainWindow.hide();
+    }
+  });
+*/
+
   // You can use 'before-quit' instead of (or with) the close event
   app.on('before-quit', function (e) {
     // Handle menu-item or keyboard shortcut quit here
     mh_shutdown();
+/*
+    if(!force_quit){
+      e.preventDefault();
+      mainWindow.hide();
+    }
+*/
   });
 
   app.on('activate-with-no-open-windows', function(){
@@ -61,22 +151,35 @@ app.whenReady().then(() => {
   });
 })
 
-// Event handler for synchronous incoming messages
-ipcMain.on('quit-message', (event, arg) => {
-  //app.quit();
-})
-
 function mh_shutdown() {
-  //console.log('Request Metaheuristic to shutdown ...');
-  const userAction = async () => {
-    const response = await fetch('http://localhost:64967/rest/v1/standalone/anon/shutdown', {
-      method: 'GET'
-    });
-    const strResponse = response.json();
-    console.log('Response from Metaheuristic: ', strResponse);
-  }
-  userAction();
+  console.log("Metaheuristic was shutdown at "+ new Date());
 
+  try {
+    (async () => {
+      try {
+        //
+        // const response = await fetch('http://example.com')
+        // const json = await response.json()
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), 2000);
+        console.log('Request Metaheuristic server to shutdown ...');
+        const response = await fetch('http://localhost:64967/rest/v1/standalone/anon/shutdown', {
+          method: 'GET',
+          signal: controller.signal
+        });
+        clearTimeout(id);
+        const strResponse = await response.text();
+        console.log('Response from Metaheuristic server:\n'+ strResponse);
+
+        log_file.end("\n");
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }
+  catch(e) {
+    console.log(e);
+  }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
