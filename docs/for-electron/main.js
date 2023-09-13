ca0284c335fd885d72c0bc35b78ae1027632c579
sergmain <serg_main@yahoo.com>
2023-09-13 00:00:15 -0700

    .

--- docs/for-electron/main.js
+++ docs/for-electron/main.js
@@ -6,91 +6,122 @@ const path = require('path')
 const http = require('http');
 const fs = require('fs');
 const util = require('util');
-// const {exec: child} = require("child_process");
+const {exec: child} = require("child_process");
+const {log} = require("util");
+const crypto = require('crypto');
 
-
-const userHomePath = path.join(app.getPath('home'), '.metaheuristic', 'electron');
-
-if (!fs.existsSync(userHomePath)){
-    fs.mkdirSync(userHomePath, { recursive: true });
+class Status {
+  stage;
+  status;
+  constructor(stage, status) {
+    this.stage = stage;
+    this.status = status;
+  }
 }
 
-const log_file = fs.createWriteStream(path.join(userHomePath, 'console.log'), {flags: 'a'});
-const log_stdout = process.stdout;
+class ElectronData {
+  electron;
+  logs;
+  status;
+  statusFile;
+  uuid;
 
-console.log = function(d) {
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
-};
+  constructor(electron, logs, status) {
+    this.electron = electron;
+    this.logs = logs;
+    this.status = status;
+  }
+}
 
+const statuses = {};
+const electronData = initElectronPath();
+const validStatusFilename = /^mh-[0-9a-zA-Z-]+.status$/;
+
+
+initMetaheuristicStatusFile();
+
+const log_file = redirectStdout();
+
+// this call of console must be after calling redirectStdout()
 console.log("Metaheuristic front-end was started at " + new Date());
 
-const port = 64968;
+startUIServer();
 
-try {
-  const hostname = 'localhost';
-// Creating Server
-  console.log(`Start preparing a Server for starting at http://${hostname}:${port}/`);
-  const server = http.createServer((req,res)=>{
-    // Handling Request and Response
-    res.statusCode=200;
-    res.setHeader('Content-Type', 'text/plain')
-    res.end("Front-end is alive.")
-  });
+startMetaheuristicServer();
 
-// Making the server to listen to required
-// hostname and port number
-  server.listen(port, hostname,()=>{
-    // Callback
-    console.log(`Server is running at http://${hostname}:${port}/`);
-  });
-} catch(e) {
-  console.log(e);
-}
+let mainWindow;
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  mainWindow = createWindow();
 
-/*
-try {
-  spawn('"with spaces.cmd"', ['arg with spaces'], { shell: true });
-
-  // const child = require('child_process').execFile;
-  const childSpawn = require('child_process').spawn;
-  let executablePath = '"'+path.join(__dirname, 'metaheuristic', 'metaheuristic.exe') + '"';
-  console.log("Metaheuristic executablePath: " + executablePath);
-
-  childSpawn('metaheuristic.exe', function(err, data) {
-    if(err){
-      console.error(err);
-      return;
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      mainWindow = createWindow()
     }
-    console.log(data.toString());
-  });
-}
-catch(e) {
-  console.log(e);
-}
-*/
-try {
-  const child = require('child_process').exec;
-  let executablePath = '"'+path.join(__dirname, 'metaheuristic', 'metaheuristic.exe') + '"';
-  console.log("Metaheuristic executablePath: " + executablePath);
+  })
 
-  child(executablePath, function(err, data) {
-    if(err){
-      console.error(err);
-      return;
-    }
-    console.log(data.toString());
+  // You can use 'before-quit' instead of (or with) the close event
+  app.on('before-quit', function (e) {
+    // Handle menu-item or keyboard shortcut quit here
+    mh_shutdown();
   });
+
+  app.on('activate-with-no-open-windows', function(){
+    mainWindow.show();
+  });
+})
+
+function initMetaheuristicStatusFile() {
+  fs.readdirSync(electronData.status).forEach(file => {
+    if (validStatusFilename.test(file)) {
+      console.log("  found a lost lock file: " + file);
+      fs.rmSync(file, {force: true});
+    }
+  });
+  electronData.uuid = crypto.randomUUID();
+  electronData.statusFile = 'mh-' + electronData.uuid + '.status';
 }
-catch(e) {
-  console.log(e);
+
+function initElectronPath() {
+  const userHomePath = app.getPath('home');
+  const metaheuristicPath = path.join(userHomePath, '.metaheuristic');
+  const electronPath = path.join(metaheuristicPath, 'electron');
+
+  if (!fs.existsSync(electronPath)) {
+    fs.mkdirSync(electronPath, {recursive: true});
+  }
+  const logPath = path.join(electronPath, 'logs');
+  if (!fs.existsSync(logPath)) {
+    fs.mkdirSync(logPath, {recursive: true});
+  }
+  const statusPath = path.join(metaheuristicPath, 'status');
+  if (!fs.existsSync(statusPath)) {
+    fs.mkdirSync(statusPath, {recursive: true});
+  }
+
+  return new ElectronData(electronPath, logPath, statusPath);
+}
+
+function redirectStdout() {
+  const log_file = fs.createWriteStream(path.join(electronData.logs, 'console.log'), {flags: 'a'});
+  const log_stdout = process.stdout;
+
+  console.log = function (d) {
+    log_file.write(util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+  };
+  return log_file;
 }
 
 function createWindow () {
   // Window Customization - https://www.electronjs.org/docs/latest/tutorial/window-customization
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     frame: false,
     titleBarStyle: 'hidden',
     fullscreen: true,
@@ -101,55 +132,81 @@ function createWindow () {
       // nodeIntegration: true,
       enableRemoteModule: false
     }
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'mh-angular', 'index.html'))
+  mainWindow.loadFile(path.join(__dirname, 'mh-angular', 'index.html'));
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
+  return mainWindow;
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
+function handleCommands(req, res) {
+  const url = req.url;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain')
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+  switch(url) {
+    case "/ping": {
+      res.end("Front-end is alive.")
+      break;
     }
-  })
-
-  // Continue to handle mainWindow "close" event here
-/*
-  mainWindow.on('close', function(e){
-    if(!force_quit){
-      e.preventDefault();
-      mainWindow.hide();
+    case "/status": {
+      console.log(""+req.method+" "+ url + " " + req.headers);
+      const currStatuses = JSON.stringify(statuses);
+      console.log("statuses: " + currStatuses);
+      res.end(currStatuses)
+      break;
     }
-  });
-*/
-
-  // You can use 'before-quit' instead of (or with) the close event
-  app.on('before-quit', function (e) {
-    // Handle menu-item or keyboard shortcut quit here
-    mh_shutdown();
-/*
-    if(!force_quit){
-      e.preventDefault();
-      mainWindow.hide();
+    default: {
+      console.log("Not supported url: " + url);
+      break;
     }
-*/
-  });
+  }
+}
 
-  app.on('activate-with-no-open-windows', function(){
-    mainWindow.show();
-  });
-})
+function startUIServer() {
+  const port = 64968;
+
+  try {
+    const hostname = 'localhost';
+// Creating Server
+    console.log(`Start preparing a Server for starting at http://${hostname}:${port}/`);
+    const server = http.createServer(
+        (req, res) => handleCommands(req, res)
+    );
+
+// Making the server to listen to required
+// hostname and port number
+    server.listen(port, hostname, () => {
+      // Callback
+      console.log(`Server is running at http://${hostname}:${port}/`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function startMetaheuristicServer() {
+  try {
+    // spawn('"with spaces.cmd"', ['arg with spaces'], { shell: true });
+    // const childSpawn = require('child_process').spawn;
+    const child = require('child_process').exec;
+    let executablePath = '"' + path.join(__dirname, 'metaheuristic', 'metaheuristic.exe') + '" uuid='+electronData.uuid;
+    console.log("Metaheuristic executablePath: " + executablePath);
+
+    child(executablePath, function (err, data) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(data.toString());
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 function mh_shutdown() {
   console.log("Metaheuristic was shutdown at "+ new Date());
